14c32aee1c866489f9d8d9dfdf182e9dd14c8d2b
Jordan <jordanmcmillan3@icloud.com>
2024-04-09 11:43:34 +0100

    added a 25m offset to calculate airgap instead of distance to sensor.

--- python/OperationMonitor.py
+++ python/OperationMonitor.py
@@ -47,7 +47,7 @@ class OperationMonitor:
 
         try:
             distance = self.lidar.getDistance() / 10  # Convert cm to m. This is using the third party lidar code to get a distance for the sensor
-            self.currentLidarAirgap = distance
+            self.currentLidarAirgap = (distance - 25.0) # *** offset for sensor locations here *** currently 25m above bottom of hull
             print(f"Current airgap distance: {distance} m")
             self.history.append(distance)
         except Exception as e:

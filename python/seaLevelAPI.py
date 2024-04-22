07a699d10393cb1e674a0edc4ede387f84e9ac9b
Jordan <jordanmcmillan3@icloud.com>
2024-04-22 15:13:21 +0100

    edited notes

--- python/seaLevelAPI.py
+++ python/seaLevelAPI.py
@@ -30,46 +30,19 @@ class SeaLevelAPI:
         
         except requests.exceptions.HTTPError as errhttp:
             print(f"Http Error: {errhttp}")
-            raise
+            
         except requests.exceptions.ConnectionError as errconnection:
             print(f"Connection Error: {errconnection}")
-            raise
+            
         except requests.exceptions.Timeout as errtimeout:
             print(f"Timeout Error: {errtimeout}")
-            raise
+            
         except requests.exceptions.RequestException as err:
             print(f"Unknown Error: {err}")
-            raise
+            
 
 
 """ 
-
-   def fetch_data(self, lat, lng, start, end):
-        headers = {'Authorization': self.api_key}
-        params = {
-            'lat': lat,
-            'lng': lng,
-            'start': start.timestamp(),
-            'end': end.timestamp(),
-        }
-        try:
-            response = requests.get(self.base_url, headers=headers, params=params)
-            response.raise_for_status()
-            return response.json()
-        
-        except requests.exceptions.HTTPError as errhttp:
-            print(f"Http Error: {errhttp}")
-        except requests.exceptions.ConnectionError as errconnection:
-            print(f"Connection Error: {errconnection}")
-        except requests.exceptions.Timeout as errtimeout:
-            print(f"Timeout Error: {errtimeout}")
-        except requests.exceptions.RequestException as err:
-            print(f"Unknown Error: {err}") 
-
-
-
-
-
 
 fetch = SeaLevelAPI('exclude/config.env')
 data = fetch.fetch_data(43.38, -3.01, arrow.now().floor('hour'), arrow.now().floor('hour').shift(hours=8))
